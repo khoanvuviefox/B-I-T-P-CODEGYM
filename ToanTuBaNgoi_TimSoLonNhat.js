92f6331c8806120f57d6fe3eaec2a059515d5e2b
Vũ Khoan <93373792+khoanvuviefox@users.noreply.github.com>
2021-11-12 08:21:55 +0700

    Update ToanTuBaNgoi_TimSoLonNhat.js

--- ToanTuBaNgoi_TimSoLonNhat.js
+++ ToanTuBaNgoi_TimSoLonNhat.js
@@ -2,5 +2,5 @@ var a = 10;
 var b = 20;
 var c = 30;
 
-var result = (a > b) ? (a > c ? a : c): (b > c ? b : c);
-System.out.println (result);
+var max = (a > b) ? (a > c ? a : c): (b > c ? b : c);
+console.log(max);

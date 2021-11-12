c8d921f0103e1b8103074597cd3019e4fb4173df
Vũ Khoan <93373792+khoanvuviefox@users.noreply.github.com>
2021-11-12 08:26:48 +0700

    Update BaNgoi_diemTB.js

--- BaNgoi_diemTB.js
+++ BaNgoi_diemTB.js
@@ -1,12 +1,12 @@
-let avg = prompt("Nhập điểm trung bình để tính học lực :", 0);
+let avg = 10;
 let kind = "";
 
 kind = (avg > 0 && avg <= 10) ?
-    (avg >= 9) ? "XS" :
-        (avg >= 8) ? "Gioi" :
-            (avg >= 7) ? "Kha" :
-                (avg >= 6) ? "TB Kha" :
-                    (avg >= 5) ? "TB" :
-                        (avg < 5) ? "Yeu" : "Diem nhap vao khong hop le"
-: "Diem nhap vao khong hop le";
+    (avg >= 9) ? "Xuất Sắc" :
+        (avg >= 8) ? "Giỏi" :
+            (avg >= 7) ? "Khá" :
+                (avg >= 6) ? "Trung bình khá" :
+                    (avg >= 5) ? "Trung bình" :
+                        (avg < 5) ? "Yếu" : "Điểm không hợp lệ"
+: "Điểm không hợp lệ";
 console.log(kind);

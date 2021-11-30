1a6a54624b45f401cbf5c415898565bef8ba01ee
Vũ Khoan <93373792+khoanvuviefox@users.noreply.github.com>
2021-11-30 12:41:10 +0700

    Update script.js

--- WEEK 4/gamelau/script.js
+++ WEEK 4/gamelau/script.js
@@ -1,31 +1,45 @@
-function Hero(image, top, left, size){
-    this.image = image;
-    this.top = top;
-    this.left = left;
-    this.size = size;
-  
-    this.getHeroElement = function(){
-      return '<img width="'+ this.size + '"' +
-        ' height="'+ this.size + '"' +
-        ' src="' + this.image +'"' +
-        ' style="top: '+this.top+'px; left:'+this.left+'px;position:absolute;" />';
-    }
-  
-    this.moveRight = function(){
-      this.left += 20;
+function Hero(image, top, left, size, speed) {
+  this.image = image;
+  this.top = top;
+  this.left = left;
+  this.size = size;
+  this.speed = speed;
+  this.getHeroElement = function () {
+      return '<img width="' + this.size + '"' +
+          ' height="' + this.size + '"' +
+          ' src="' + this.image + '"' +
+          ' style="top: ' + this.top + 'px; left:' + this.left + 'px;position:absolute;" />';
+  };
+  this.moveRight = function () {
+      this.left += 30;
       console.log('ok: ' + this.left);
-    }
-  
+  };
+  this.moveDown = function () {
+      this.top += 30;
+      console.log('ok: ' + this.top);
+  };
+  this.moveLeft = function () {
+      this.left -= 30;
+      console.log('ok: ' + this.left);
   }
-  
-  var hero = new Hero('doremon.png', 20, 30, 200);
-  
-  function start(){
-    if(hero.left < window.innerWidth - hero.size){
+  this.moveUp = function () {
+      this.top -= 30;
+      console.log('ok: ' + this.top);
+  }
+}
+var hero = new Hero('doremon.png', 20, 30, 100);
+function start() {
+  if (hero.left < window.innerWidth - hero.size && hero.top == 20) {
       hero.moveRight();
-    }
-    document.getElementById('game').innerHTML = hero.getHeroElement();
-    setTimeout(start, 500)
+  } else if (hero.left > window.innerWidth - hero.size && window.innerHeight - hero.size > hero.top) {
+      hero.moveDown();
+  } else if (window.innerHeight - hero.size < hero.top) {
+      hero.moveLeft();
   }
-  
-  start();
+  if (hero.left === 0) {
+      hero.moveUp();
+  }
+  document.getElementById('game').innerHTML = hero.getHeroElement();
+  setTimeout(start, 50)
+}
+start();
